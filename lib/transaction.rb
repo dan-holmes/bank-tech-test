b7806c9fa9269f3c8c320d12aef4941e39d8b023
Dan Holmes <dh@danielholmes.com>
2020-11-30 20:15:24 +0000

    Replace .type method with .debit_formatted and .credit formatted

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -4,13 +4,31 @@ class Transaction
   attr_reader :value, :updated_balance, :date
 
   def initialize(value:, updated_balance:, date: Date.today)
-    @value = value
-    @updated_balance = updated_balance
+    @value = value.to_f
+    @updated_balance = updated_balance.to_f
     @date = date
   end
 
-  def type
-    return :debit if @value > 0
-    return :credit if @value < 0
+  def debit_formatted
+    format_currency(debit)
+  end
+
+  def credit_formatted
+    format_currency(credit)
+  end
+
+  private
+
+  def debit
+    value if value > 0
+  end
+
+  def credit
+    -value if value < 0
+  end
+
+  def format_currency(float)
+    return nil if float == nil
+    "%.2f" % float
   end
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -2,22 +2,38 @@ require "transaction"
 
 describe Transaction do
   describe ".initialize" do
-    it "stores a value and updated_balance balance" do
+    it "stores a value and updated_balance" do
       transaction = Transaction.new(value: 10, updated_balance: 50)
       expect(transaction.value).to eq 10
       expect(transaction.updated_balance).to eq 50
     end
   end
 
-  describe ".type" do
-    it "returns :debit if value is positive" do
-      transaction = Transaction.new(value: 10, updated_balance: 50)
-      expect(transaction.type).to eq :debit
+  context "transaction value positive" do
+    let(:transaction) { Transaction.new(value: 10, updated_balance: 50) }
+    describe ".debit_formatted" do
+      it "returns a formatted, positive amount" do
+        expect(transaction.debit_formatted).to eq "10.00"
+      end
     end
+    describe ".credit_formatted" do
+      it "returns nil" do
+        expect(transaction.credit_formatted).to be_nil
+      end
+    end
+  end
 
-    it "returns :credit if value is negative" do
-      transaction = Transaction.new(value: -10, updated_balance: 50)
-      expect(transaction.type).to eq :credit
+  context "transaction value negative" do
+    let(:transaction) { Transaction.new(value: -10, updated_balance: 50) }
+    describe ".debit_formatted" do
+      it "returns nil" do
+        expect(transaction.debit_formatted).to be_nil
+      end
+    end
+    describe ".credit_formatted" do
+      it "returns a formatted, positive amount" do
+        expect(transaction.credit_formatted).to eq "10.00"
+      end
     end
   end
 
