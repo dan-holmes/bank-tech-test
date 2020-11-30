47b1015b1de6e034784e086d46d674a4acb16a02
Dan Holmes <dh@danielholmes.com>
2020-11-30 14:14:21 +0000

    Set initial balance as 0.

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,4 +1,10 @@
 require "account"
 
 describe Account do
+  describe "initialize" do
+    let(:account) { Account.new }
+    it "sets balance to 0" do
+      expect(account.balance).to eq 0
+    end
+  end
 end
