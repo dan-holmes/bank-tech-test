8e20785b9f52947984732d983ac37fe7ad2948bb
Dan Holmes <dh@danielholmes.com>
2020-12-02 14:29:15 +0000

    Remove test for constructor;

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -5,15 +5,6 @@ describe Account do
   let(:transaction) { double(:transaction) }
   let(:transaction_class) { double(:transaction_class, new: transaction) }
 
-  describe ".initialize" do
-    it "sets balance to 0" do
-      expect(account.balance).to eq 0
-    end
-    it "creates an empty list of transactions" do
-      expect(account.transactions).to be_empty
-    end
-  end
-
   describe ".deposit" do
     it "increases the balance by the given amount" do
       expect { account.deposit(10) }.to change { account.balance }.by(10)
